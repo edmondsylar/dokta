9ea990dd44f009bba27c85e1d7e8b014e5fc6e3f
ukwelys <ukwelys@gmail.com>
2020-02-11 05:33:39 -0800

    frontend setup in progress

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<header class="header_sticky">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-6">
+        <div id="logo_home">
+          <h1><a href="search.php" title="Dokta">Dokta</a></h1>
+        </div>
+      </div>
+      <nav class="col-lg-9 col-6">
+        <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="#0"><span>Menu mobile</span></a>
+        <ul id="top_access">
+
+        </ul>
+        <div class="main-menu">
+          <ul>
+            <li><a href="index.php">Home</a></li>
+          </ul>
+        </div>
+        <!-- /main-menu -->
+      </nav>
+    </div>
+  </div>
+  <!-- /container -->
+</header>
