6b3b07aeb4a4a4f6fb38fbb1963674dac9c5fae9
ukwelys <ukwelys@gmail.com>
2020-02-13 05:35:40 -0800

    maps push

--- details.php
+++ details.php
@@ -88,7 +88,7 @@
        </div>
        <aside class="col-xl-4 col-lg-4" id="sidebar">
          <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
-           <iframe src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+           <iframe src="https://maps.google.com/maps?q=<?php echo $value['address']; ?>&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
              style="border:0" allowfullscreen></iframe>
          </div>
 
