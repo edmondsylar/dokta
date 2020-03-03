77d1123b041459b9b0393b1f2ce3a084bdb6bb5d
ukwelys <ukwelys@gmail.com>
2020-03-03 04:49:20 -0800

    added login for creating medicine

--- includes/header.php
+++ includes/header.php
@@ -3,9 +3,7 @@
     <div class="row">
       <div class="col-lg-3 col-6">
           <h3><a href="index.php" title="Dokta">Dokta</a></h3>
-        <!-- <div id="logo_home">
-          <h1><a href="search.php" title="Dokta">Dokta</a></h1>
-        </div> -->
+        <a target="_blank" href="http://157.245.221.99/apps/doc/login.php"  >Login</a>
       </div>
       <nav class="col-lg-9 col-6">
         <!-- <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="#0"><span>Menu mobile</span></a> -->
