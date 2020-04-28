0bd46bd7a2f7f534dd4db7e7886b3617795528ae
edmondsylar <ukwelys@gmail.com>
2020-04-28 00:44:40 -0700

    fixed login button view

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,11 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-3 col-6">
-          <h3><a href="index.php" title="Dokta">Dokta</a></h3>
-        <a href="./admin/"  >Login</a>
+          <h3><a href="index.php" title="Dokta">Dokta</a>
+            <span>
+            <a style="font-size: 15px; color: #3F4079;" href="./admin/"  >Login</a>
+          </span>
+      </h3>
       </div>
       <nav class="col-lg-9 col-6">
         <!-- <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="#0"><span>Menu mobile</span></a> -->
